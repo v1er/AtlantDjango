60c2a266b8959b4a965d2646d82f5fad7fe09ed8
V1eR <gangvad91@gmail.com>
2022-10-17 12:33:56 +0300

    добавив конзину

--- src/main/settings.py
+++ src/main/settings.py
@@ -69,9 +69,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart', 
             ],
         },
-    },
+    }, 
 ]
 
 WSGI_APPLICATION = 'main.wsgi.application'
